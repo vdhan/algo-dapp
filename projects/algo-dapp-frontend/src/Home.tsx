f9681dbcf702822d2e1de521f1257cf79278cb8a
An Vu <anvudh@gmail.com>
2024-08-28 22:07:50 +0700

    update frontend

--- projects/algo-dapp-frontend/src/Home.tsx
+++ projects/algo-dapp-frontend/src/Home.tsx
@@ -3,27 +3,42 @@ import { useWallet } from '@txnlab/use-wallet'
 import React, { useState } from 'react'
 import AppCalls from './components/AppCalls'
 import ConnectWallet from './components/ConnectWallet'
+import MethodCall from './components/MethodCall'
 import Transact from './components/Transact'
+
+import * as algokit from '@algorandfoundation/algokit-utils'
+import { DappClient } from './contracts/Dapp'
+import * as methods from './methods'
+import { getAlgodConfigFromViteEnvironment } from './utils/network/getAlgoClientConfigs'
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
+  algokit.Config.configure({populateAppCallResources: true})
+
   const [openWalletModal, setOpenWalletModal] = useState<boolean>(false)
   const [openDemoModal, setOpenDemoModal] = useState<boolean>(false)
   const [appCallsDemoModal, setAppCallsDemoModal] = useState<boolean>(false)
-  const { activeAddress } = useWallet()
+  const { activeAddress, signer } = useWallet()
+  let [appId, setAppId] = useState<number>(0)
+  let [unitaryPrice, setUnitaryPrice] = useState<bigint>(0n)
+  let [assetId, setAssetId] = useState<bigint>(0n)
+  let [quantity, setQuantity] = useState<bigint>(0n)
+  let [seller, setSeller] = useState<string>('')
+  let [unitLeft, setUnitLeft] = useState<bigint>(0n)
 
   const toggleWalletModal = () => {
     setOpenWalletModal(!openWalletModal)
   }
 
-  const toggleDemoModal = () => {
-    setOpenDemoModal(!openDemoModal)
-  }
-
-  const toggleAppCallsModal = () => {
-    setAppCallsDemoModal(!appCallsDemoModal)
-  }
+  let algodConfig = getAlgodConfigFromViteEnvironment()
+  let algorand = algokit.AlgorandClient.fromConfig({algodConfig})
+  algorand.setDefaultSigner(signer)
+  let dmClient = new DappClient({
+    resolveBy: 'id',
+    id: appId,
+    sender: {addr: activeAddress!, signer}
+  }, algorand.client.algod)
 
   return (
     <div className="hero min-h-screen bg-teal-400">
@@ -33,7 +48,8 @@ const Home: React.FC<HomeProps> = () => {
             Welcome to <div className="font-bold">AlgoKit 🙂</div>
           </h1>
           <p className="py-6">
-            This starter has been generated using official AlgoKit React template. Refer to the resource below for next steps.
+            This starter has been generated using official AlgoKit React template. Refer to the resource below for next
+            steps.
           </p>
 
           <div className="grid">
@@ -50,6 +66,50 @@ const Home: React.FC<HomeProps> = () => {
             <button data-test-id="connect-wallet" className="btn m-2" onClick={toggleWalletModal}>
               Wallet Connection
             </button>
+
+            <label className="label">App Id</label>
+            <input
+              type="number"
+              value={appId}
+              min={0}
+              className="input input-bordered"
+              onChange={(e) => {
+                setAppId(e.currentTarget.valueAsNumber)
+              }}
+            />
+
+            {
+              activeAddress && appId === 0 && (
+                <div>
+                  <label className="label">Unitary Price</label>
+                  <input
+                    type="number"
+                    value={(unitaryPrice / BigInt(1e6)).toString()}
+                    className="input input-bordered"
+                    onChange={(e) => {
+                      setUnitaryPrice(BigInt((e.currentTarget.valueAsNumber || 0) * 1e6))
+                    }}
+                  />
+
+                  <label className="label">Asset</label>
+                  <input
+                    type="number"
+                    value={quantity.toString()}
+                    className="input input-bordered"
+                    onChange={(e) => {
+                      setQuantity(BigInt(e.currentTarget.valueAsNumber))
+                    }}
+                  />
+
+                  <MethodCall
+                    methodFunction={methods.create(
+                      algorand, dmClient, activeAddress!, unitaryPrice, quantity, assetId, setAppId
+                    )}
+                    text='Create Application'
+                  />
+                </div>
+              )
+            }
           </div>
 
           <ConnectWallet openModal={openWalletModal} closeModal={toggleWalletModal} />
